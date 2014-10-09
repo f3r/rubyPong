04d026a76dc811bf51d42afda78be50f05004f33
Fer Martin <fernandomartinsantana@gmail.com>
2014-10-09 22:33:09 +0800

    Add score text

--- pong.rb
+++ pong.rb
@@ -3,7 +3,7 @@ require "hasu"
 Hasu.load "ball.rb"
 
 class Pong < Hasu::Window
-  
+
   WIDTH       = 768
   HEIGHT      = 576
 
@@ -13,10 +13,18 @@ class Pong < Hasu::Window
 
   def reset
     @ball = Ball.new
+
+    @left_score  = 0
+    @right_score = 0
+
+    @font = Gosu::Font.new(self, "Arial", 30)
   end
 
   def draw
     @ball.draw(self)
+
+    @font.draw(@left_score,  30,       30, 0) # top-left corner
+    @font.draw(@right_score, WIDTH-50, 30, 0) # top-right corner
   end
 
   def update
